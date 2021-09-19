cb4d25ec0c86b634e50f612115b8ac8dce54b3d0
Hema Priya Velaga <hemapriyavelaga@gmail.com>
2021-09-19 18:56:16 +0530

    Custom DaoException

---
+++ Java/Day-05/Assignments/Entities/DaoException.java
@@ -0,0 +1,15 @@
+package com.target.training.assignment.entities;
+
+public class DaoException extends Exception {
+
+    public DaoException() {
+    }
+
+    public DaoException(String message) {
+        super(message);
+    }
+
+    public DaoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
